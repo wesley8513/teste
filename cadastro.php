71955dc83c754da21333c0f72dbc052302f73d8c
wesley8513 <wesley8513@yahoo.com>
2020-05-14 17:31:23 -0300

    Form changes and db validation

--- cadastro.php
+++ cadastro.php
@@ -6,7 +6,9 @@ $servico= $_POST['servico'];
 $login= $_POST['login'];
 $senha= $_POST['senha'];
 
-$sql="INSERT INTO servicos (servico, login, senha) VALUES ('$_POST[servico]', '$_POST[login]', '$_POST[senha]')";
-$res=mysqli_query($conn, $sql);
+	$sql="INSERT INTO servicos (servico, login, senha) VALUES ('$_POST[servico]', '$_POST[login]', '$_POST[senha]')";
+	$res=mysqli_query($conn, $sql);
 
 //continuar...
+	if($res)
+		echo "Cadastro realizado com sucesso!";
--- conn.php
+++ conn.php
@@ -12,5 +12,11 @@
 		if($conn->connect_error)
 			die("Conexão falhou." . $conn->connect_error);
 		
-		
+		//checar se banco existe
+		$exist="SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = 'teste'";
+		if(!$exist)
+			$exist="CREATE DATABASE IF NOT EXISTS <teste>";
+			$q=mysqli_query($conn, $exist);
+
+			//checar essa validação (criou a tabela, mas e o banco?)
 
